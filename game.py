e1bb325c97cec71d03ec7fea22a350d428f9d887
UL7RAGAMER <siddkum@outlook.com>
2023-08-09 16:19:09 +0530

    v5.1

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ print('Welcome to U7 world')
 import gam
 import Gambling
 end_game = False
-
+print('Commands are\n- Gamble\n- Job\n- Stop')
 
 while end_game == False:
     cm = input(f'You have {Gambling.coins} coins \nYou want to Gamble or Job \n')
