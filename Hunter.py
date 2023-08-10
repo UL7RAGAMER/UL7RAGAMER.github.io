3c4ec8934afa4903fb30a7b312baf969ebdc4880
U7VE <141698048+U7VE@users.noreply.github.com>
2023-08-10 12:29:26 +0530

    Add files via upload

---
+++ Hunter.py
@@ -0,0 +1,8 @@
+import Data
+
+def Hunter():
+    mythcre = ["Golems"] * 30 ,[ "Cyclops"]* 30, ["Onis"] * 10, ["Ogres"] *10, ["Dragon"] * 5, ['Giffin'] * 15
+    mythdrop =['Rock', 'Eyes', 'Teeth', 'Skin', 'Scales', 'Feathers']
+    print('Welcome to the U7 adventure guild')
+    print('The')
+    
